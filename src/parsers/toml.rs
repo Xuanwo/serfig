f5c061db74aca5522ee74f1a20c3ba903818b724
Xuanwo <github@xuanwo.io>
2023-01-29 10:48:56 +0800

    Bump toml
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/parsers/toml.rs
+++ src/parsers/toml.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use serde::de::DeserializeOwned;
 
 use crate::Parser;
@@ -9,6 +9,8 @@ pub struct Toml;
 
 impl Parser for Toml {
     fn parse<T: DeserializeOwned>(&mut self, bs: &[u8]) -> Result<T> {
-        Ok(toml::from_slice(bs)?)
+        let s = std::str::from_utf8(bs)
+            .map_err(|err| anyhow!("input value is not valid utf-8: {err:?}"))?;
+        Ok(toml::from_str(s)?)
     }
 }
